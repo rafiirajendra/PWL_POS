462128449aec0705472ff0e31527f72e0d955b22
rafiirajendra <rafi53417@gmail.com>
2025-03-04 20:43:03 +0700

    Praktikum 1  - $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,16 +11,12 @@ class UserController extends Controller
     public function index(){
         // tambah data user dengan Eloquent Model
         $data = [
-            // 'username' => 'customer-1',
-            // 'nama' => 'Pelanggan',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 4
-
-            'nama'=>'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
-
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
