8064d5bd8ff8223ca7342697d808ad798164fcec
rafiirajendra <rafi53417@gmail.com>
2025-03-05 08:38:24 +0700

    Praktikum 2.5 – Attribute Changes

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>Data User</title>
 </head>
 <body>
     <h1>Data User</h1>
-    <table border="1">
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
         </tr>
         <tr>
-            <td>{{ $data }}</td> 
+            <td>{{$data->user_id}}</td>
+            <td>{{$data->username}}</td>
+            <td>{{$data->nama}}</td>
+            <td>{{$data->level_id}}</td>
         </tr>
     </table>
 </body>
