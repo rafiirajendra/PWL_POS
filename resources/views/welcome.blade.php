6a7ef2f3d86febed39d1f345beb2e100adf41f58
rafiirajendra <rafi53417@gmail.com>
2025-03-12 09:27:05 +0700

    modifikasi welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,17 @@
 
 <div class="card">
     <div class="card-header">
-        <h3 class="card-title">Halo, apakabar!!!</h3>
+        <h3 class="card-title">Halo, cuy apakabar!!!</h3>
         <div class="card-tools"></div>
     </div>
     <div class="card-body">
-        Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
+        Selamat datang semua, ini adalah halaman utama dari aplikasi ini.<br>
+        CIHUYYY!!!!!!!!!  
+        <br><br>
+        <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWJzNDFyeTQ0YndzbWV0M2k5aXV3ZnJ2d2NiMThuZHRrc3J2dTE0ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2zUn8hAwJwG4abiS0p/giphy.gif" 
+             alt="Gif Cat" 
+             class="img-fluid">
     </div>
 </div>
+
 @endsection
