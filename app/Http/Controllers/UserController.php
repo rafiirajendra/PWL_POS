a619bf73f383b69722a06f71c0ffb02aed2cb756
rafiirajendra <rafi53417@gmail.com>
2025-03-19 09:39:44 +0700

    Praktikum 1. Modal Ajax Tambah Data (Data User)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 use function PHPSTORM_META\map;
 
@@ -240,5 +241,39 @@ class UserController extends Controller
         } catch (\Illuminate\Database\QueryException $e) { // Jika gagal delete (biasanya FK constraint)
             return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
-    } 
+    }
+
+    public function create_ajax() {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+        return view('user.create_ajax')->with('level', $level);
+    }
+
+    public function store_ajax(Request $request) {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama' => 'required|string|max:100',
+                'password' => 'required|min:6',
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi gagal',
+                    'msgfield' => $validator->errors()
+                ]);
+            }
+    
+            UserModel::create($request->all());
+            return response()->json([
+                'status' => true,
+                'success' => 'Data user Berhasil disimpan'
+            ]);
+    
+        }
+        redirect('/');
+    }
 }
